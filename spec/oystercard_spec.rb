8d6a6df8b20795720564904abb01476e289a3b0e
Simone <simonewlkr@hotmail.com>
2019-11-05 16:30:40 +0000

    Unit test for existance of oystercard class and balance method added and passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,15 @@
-#require "./lib/oystercard.rb"
+require 'oystercard'
 
-describe Oyster do
+describe Oystercard do
+  it 'Creates an instance' do
+    oyster_card = Oystercard.new
+  expect(oyster_card).to be_instance_of(Oystercard)
+  end
 
-end
+#Checks that attribute accessor balance is responsive in oystercard folder
+   it 'does it respond to balance' do
+    oyster_card = Oystercard.new
+   expect(subject).to respond_to(:balance)
+   end
+
+ end
