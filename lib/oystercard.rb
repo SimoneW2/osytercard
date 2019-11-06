a70be28fad72c3ea7a15559874a7a31b12a9b5b3
Simone <simonewlkr@hotmail.com>
2019-11-06 16:10:28 +0000

    unit test for deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,18 @@
 class Oystercard
-  attr_accessor :balance #This is equivalent to writting a balance method that is empty
+attr_accessor :balance #This is equivalent to writting a balance method that is empty
 #This initializes a instance variable which we give zero as we want balance to equal zero
 
+MAXIMUM_BALANCE = 90 #gives the constant a value of 90
   def initialize
     @balance = 0
   end
 
    def top_up(amount)
+     #can replace raise with fail and it will do that same thing.
+     raise "Maximum balance of £ #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
      @balance += amount
+   end
+
+   def deduct(amount)
    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -28,8 +28,13 @@ describe Oystercard do
  end
 
  it 'raises an error if the maximum balance is exceeded' do
-   maximum_balance = Oystercard::MAXIMUM_BALANCE
-   subject.top_up(maximum_balance)
-   expect{subject.top_up 1}.to raise_error 'Maximum balance exceeded'
+   maximum_balance = Oystercard::MAXIMUM_BALANCE #The class::CONSTANT means that constant is inside the class the variable stores this message
+   subject.top_up(maximum_balance) #subject is keyword
+  expect{subject.top_up 1}.to raise_error "Maximum balance of £ #{maximum_balance} exceeded" #Needs double quotes to use write the variable
  end
+
+  it 'checks that the class Oystercard respond to method deduct' do
+    oyster_card = Oystercard.new
+   expect(subject).to respond_to(:deduct).with(1).argument
+   end
 end
