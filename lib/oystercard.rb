d6025e2ab6eece4839aa11d89e9377d4239e0c5c
Simone <simonewlkr@hotmail.com>
2019-11-06 15:08:27 +0000

    spec file test for maximum balance added. Failed test because constant not added to production file

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,12 @@
 class Oystercard
-  attr_accessor :balance
+  attr_accessor :balance #This is equivalent to writting a balance method that is empty
 #This initializes a instance variable which we give zero as we want balance to equal zero
+
   def initialize
     @balance = 0
   end
 
    def top_up(amount)
+     @balance += amount
    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,8 +17,19 @@ describe Oystercard do
      expect(subject.balance).to eq(0)
     end
 
-    it 'it respond to top up method' do
+    it 'responds to top_up method with one arguement' do
       oyster_card = Oystercard.new
-    expect(subject).to respond_to(:top_up).with(1).argument
+    expect(subject).to respond_to(:top_up).with(1).argument #Subject is a keyword for the current instance in this case meaning oyster_card
   end
+
+  it 'adds money to balance' do
+    oyster_card = Oystercard.new
+    expect{subject.top_up 1}.to change{subject.balance}.by 1
  end
+
+ it 'raises an error if the maximum balance is exceeded' do
+   maximum_balance = Oystercard::MAXIMUM_BALANCE
+   subject.top_up(maximum_balance)
+   expect{subject.top_up 1}.to raise_error 'Maximum balance exceeded'
+ end
+end
